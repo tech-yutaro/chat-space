f41100ca4539c3d5dd4f7b20e25a25b740b83928
tech-yutaro <tanabeyutaro89@gmail.com>
2020-03-27 21:24:39 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,9 +25,9 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+# デプロイ処理が終わった後、Unicornを再起動するための記述
 set :linked_files, %w{ config/secrets.yml }
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
