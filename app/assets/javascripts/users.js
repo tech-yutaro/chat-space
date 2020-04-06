8a0d9a6573161b48084e8f9355d3adf3a2e7b1ef
tech-yutaro <tanabeyutaro89@gmail.com>
2020-04-06 22:06:53 +0900

    commit

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -17,7 +17,6 @@ $(function() {
     `;
     $("#user-search-result").append(html);
   }
-
   function addDeleteUser(name, id) {
     let html = `
     <div class="chat-group-user clearfix" id="${id}">
@@ -56,13 +55,14 @@ $(function() {
       });
   });
   $(document).on("click", ".chat-group-user__btn--add", function() {
+    console.log
     const userName = $(this).attr("data-user-name");
     const userId = $(this).attr("data-user-id");
     $(this)
       .parent()
       .remove();
     addDeleteUser(userName, userId);
-    addMember(userId)
+    addMember(userId);
   });
   $(document).on("click", ".chat-group-user__btn--remove", function() {
     $(this)
